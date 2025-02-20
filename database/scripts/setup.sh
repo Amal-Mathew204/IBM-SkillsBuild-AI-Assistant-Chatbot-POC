4b8531055a87df72c6e33a4cc19e539af254e450
aamirnaje <120026539+aamirnaje@users.noreply.github.com>
2025-02-20 13:36:43 +0000

    -Remove whitespace

--- database/scripts/setup.sh
+++ database/scripts/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Create the user
 mongosh --host $MONGO_HOST --port $MONGO_PORT -u $MONGO_ROOT_USERNAME -p $MONGO_ROOT_PASSWORD --authenticationDatabase admin <<EOF
 use admin
@@ -9,5 +8,4 @@ db.createUser({
   roles: [{ role: "readWrite", db: "$MONGO_CHATBOT_DATABASE" }]
 })
 EOF
-
 echo "User $MONGO_USER created successfully in database admin"
