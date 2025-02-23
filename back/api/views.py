56fa770761aa030001535d3cf923dc6ebc9201c2
Amal-Mathew204 <amalmathew2004@outlook.com>
2025-02-23 20:43:42 +0000

    Add Semantic Search Module Implementation
    This is a callable method for performing a semantic search (through the semantic search module) to obtain the top k courses relevant to the user's query.
    
    The function handles the initialisation of the embedding model object and MongoDatabase connection objects.

--- back/api/views.py
+++ back/api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+"""
+Module contains all the view functions for the api app
+"""
+import os
+from .WIPSemanticSearchModule.sts_module.embedding_module.controller import EmbeddingController
+from .WIPSemanticSearchModule.sts_module.database.mongo_db_interface import MongoDBDatabase
 
 # Create your views here.
+
+def get_top_k_courses(query: str, k: int) -> list[dict]:
+    """
+    Method calls the semantic search module to calling the `courses_semantic_search`
+    to obtain the top k courses with the greatest semantic relation to the users query.
+
+    Args: 
+        query (str): User Input query to obtain courses
+        k (int): Top number of courses to be returned by the semantic search module
+
+    Returns: 
+        list[dict]: list of top k courses (information stored as a dict object)
+    """
+    url: str = f"mongodb://{os.getenv('MONGO_CONTAINER')}:{os.getenv('MONGO_PORT')}/"
+    #Database Object connecting to the Courses Collection
+    courses_database = MongoDBDatabase(
+                url=url,
+                username=os.getenv('MONGO_USER'),
+                password=os.getenv('MONGO_PASSWORD'),
+                auth_mechanism=os.getenv('MONGO_AUTH_MECHANISM'),
+                database=os.getenv('MONGO_CHATBOT_DATABASE'),
+                collection=os.getenv('MONGO_COURSE_COLLECTION'))
+    #Database Object connecting to the Embedded Dataset Collection
+    embedded_database = MongoDBDatabase(
+                    url=url,
+                    username=os.getenv('MONGO_USER'),
+                    password=os.getenv('MONGO_PASSWORD'),
+                    auth_mechanism=os.getenv('MONGO_AUTH_MECHANISM'),
+                    database=os.getenv('MONGO_CHATBOT_DATABASE'),
+                    collection=os.getenv('MONGO_EMBEDDED_DATASET_COLLECTION'))
+    controller = EmbeddingController(courses_database, embedded_database)
+    return controller.courses_semantic_search(query, k)
