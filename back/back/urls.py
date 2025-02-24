ee1a95a040f5a52144736ec2fba60c8b023d51d3
Amal-Mathew204 <amalmathew2004@outlook.com>
2025-02-24 13:39:59 +0000

    Fix View Function URL

--- back/back/urls.py
+++ back/back/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('chatbotResonse/', views.chatbotResponse, name='chatbotResponse'),
+    path('', include('api.urls')),
 ]
