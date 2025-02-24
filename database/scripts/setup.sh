7250006c33beb18faf529533838807c9dad874f8
Amal-Mathew204 <amalmathew2004@outlook.com>
2025-02-24 09:31:30 +0000

    Change .env

--- database/scripts/setup.sh
+++ database/scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create the user
-mongosh --host $MONGO_HOST --port $MONGO_PORT -u $MONGO_ROOT_USERNAME -p $MONGO_ROOT_PASSWORD --authenticationDatabase admin <<EOF
+mongosh --host $HOST --port $MONGO_PORT -u $MONGO_ROOT_USERNAME -p $MONGO_ROOT_PASSWORD --authenticationDatabase admin <<EOF
 use admin
 db.createUser({
   user: "$MONGO_USER",
