2ce684967d460abd24c68017c8402b906cd08cbd
Mohamed Zahidur Rahman <zahidur2104@gmail.com>
2025-03-03 15:20:42 +0000

    Sessions Backend

--- back/api/views.py
+++ back/api/views.py
@@ -2,10 +2,10 @@
 Module contains all the view functions for the api app
 """
 import requests
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 
 
-def chatbot(request, query: str, k: int) -> JsonResponse:
+def chatbot(request: HttpRequest, query: str, k: int) -> JsonResponse:
     """
     Method
     """
@@ -16,6 +16,16 @@ def chatbot(request, query: str, k: int) -> JsonResponse:
     # put ports in env files to
     # also do exception handling cause ibr the semantic search container is slow
     # (so making early requests will cause exceptions)
+
+    if not request.session.session_key:
+        request.session.create()
+        request.session["input_count"]=0
+        
+    request.session["input_count"] = request.session["input_count"] + 1
+    print("REQUEST SESSION",request.session["input_count"])
+    
+
+
     url: str = f"http://nginx:80/search/{query}/{k}"
     print(url)
     response = requests.get(url, timeout=30)
