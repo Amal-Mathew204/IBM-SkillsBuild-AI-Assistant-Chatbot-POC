12aa3deada790b65dd449bfbcc3dbc576444d278
Mohamed Zahidur Rahman <zahidur2104@gmail.com>
2025-03-18 15:49:20 +0000

    api endpoints

--- back/api/urls.py
+++ back/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('chatbot/<str:query>/<int:k>', views.chatbot),
     path('similarcourses/', views.get_similar_courses),
+    path('resetchat/', views.reset_chat),
+    path('fetchchat/', views.fetch_chat),
 ]
